5df2a5baad9e036f183766d1c26a4c4c8a1dfd65
Its-Oliveira <oliveira.gctt@gmail.com>
2026-03-08 18:48:57 -0300

    Update teste.py

--- PI/teste.py
+++ PI/teste.py
@@ -1,19 +1,19 @@
-def tabuleiro(n:int) -> str:
-    tabuleiro_completo = ''
-    for i in range(n):
-        linha = ''
-        for j in range(n):
-                if (i + j) % 2 == 0:
-                    linha = linha + 'o'
-                else:
-                    linha = linha + '*'
-        tabuleiro_completo += linha + '\n'
-        
-    return tabuleiro_completo
-        
-def main():
+ver = 1
+while ver != 0:
     numero = int(input())
-    resultado = tabuleiro(numero)
-    print(resultado)
+    if numero != 0:
+        soma_divisores = 0 
+        for i in range(1,numero):
+            if numero % i == 0:
+                soma_divisores += i
 
-main()
+        if soma_divisores == numero:
+            print(f'{numero} é perfeito')
+        else:
+            print(f'{numero} não é perfeito')
+
+    else: 
+        ver = 0
+
+
+ 
